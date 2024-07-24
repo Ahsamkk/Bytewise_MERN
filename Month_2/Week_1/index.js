cd01587d2cbdfe196403e8d28b1303390c02ac8e
AhsamKaimkhani <ahsamkk12@gmail.com>
2024-07-24 17:13:29 +0500

    Updated Files

--- Month_2/Week_1/index.js
+++ Month_2/Week_1/index.js
@@ -1,18 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import usersRoutes from "./routes/users.js";
+
 const app = express();
 
-// Middleware
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+app.use("/people", usersRoutes);
+app.get("/", (req, res) => res.send("Welcome to the Users API!"));
+app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
 
-app.listen(5000, () => console.log("Server Started"))
-
-
-app.get('/', (req,res) => {
-  console.log("something")
-  res.send("hello homepage")
-})
-
-
+app.listen(5000, () => console.log("Server running on port:5000"));
