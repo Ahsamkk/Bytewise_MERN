25342622996a019549f10afd9119f2775e972352
AhsamKaimkhani <ahsamkk12@gmail.com>
2024-07-30 00:05:41 +0500

    Porject Completed

---
+++ Month_2/Week_2/youtube-clone/src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: '50'
+  },
+  headers: {
+    'x-rapidapi-key': '44cc7a6fc2msh993222c80d86e6ep11dea2jsn54d2c33883db',
+    'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+export const fetchFromAPI = async (url) => {
+  const {data} = await axios.get(`${BASE_URL}/${url}`,options)
+
+  return data;
+}
